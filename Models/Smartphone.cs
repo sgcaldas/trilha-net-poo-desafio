8b735700ceb2c2ad4071968a53c4c62ec2f53845
Sonia <caldasgsonia@gmail.com>
2024-01-04 23:07:08 -0300

    Código do desafio completado e nutrido com novos métodos

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,24 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public Smartphone()
+        {
 
-        public Smartphone(string numero)
+        }
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
+
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
         public void Ligar()
         {
@@ -18,9 +28,17 @@ namespace DesafioPOO.Models
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo ligação...♫♪");
         }
 
+        public void AlterarNumero()
+        {
+            Console.WriteLine($"Seu novo número é {Numero}");
+        }
+        public void ExibirDados()
+        {
+            Console.WriteLine($"O seu celular tem modelo {Modelo}, IMEI {IMEI}, memória de {Memoria} GB e número {Numero}");
+        }
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "991999999", modelo: "2280", imei: "123456789", memoria: 1);
+nokia.ExibirDados();
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Trail Biker");
+nokia.Numero = "991299999";
+nokia.AlterarNumero();
+nokia.ExibirDados();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone (numero: "981899999", modelo: "5", imei: "456789123", memoria: 16);
+nokia.ExibirDados();
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Mario Kart Tour");
